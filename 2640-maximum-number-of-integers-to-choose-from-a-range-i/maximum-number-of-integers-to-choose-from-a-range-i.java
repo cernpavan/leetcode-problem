3040019ad6bb39d08bdc7cb48356507648edbbfc
cernpavan <gunupatipavankumar@gmail.com>
2024-12-06 15:55:28 +0530

    Time: 52 ms (26.63%) | Memory: 46.5 MB (5.00%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -5,19 +5,14 @@ class Solution {
         for (int num : banned) {
             bannedMap.put(num, true);
         }
-
         int sum = 0;
         int count = 0;
-
-
         for (int i = 1; i <= n; i++) {
-      
             if (!bannedMap.containsKey(i) && sum + i <= maxSum) {
                 sum += i;
                 count++;
             }
         }
-
         return count;
     }
 }
