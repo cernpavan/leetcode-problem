7dd276689e2282fb7005ad7414664c0197033856
cernpavan <gunupatipavankumar@gmail.com>
2025-01-03 08:18:55 +0530

    Time: 2 ms (100.00%) | Memory: 57.1 MB (94.00%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,24 +1,45 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
+        // int len=nums.length;
+        // long[] prefix=new long[len];
+        // long[] suffix=new long[len];
+        // long presum=0;
+        // for(int i=0;i<len;i++){
+        //     presum+=nums[i];
+        //     prefix[i]=presum;
+        // }
+
+        // long sufsum=0;
+        // for(int i=len-1;i>=0;i--){
+        //     sufsum+=nums[i];
+        //     suffix[i]=sufsum;
+        // }
+        // int validSplits=0;
+        // for(int i=0;i<len-1;i++){
+        //     if(prefix[i]>=suffix[i+1]){
+        //         validSplits++;
+        //     }
+        // }
+        // return validSplits;
+
         int len=nums.length;
-        long[] prefix=new long[len];
-        long[] suffix=new long[len];
-        long presum=0;
-        for(int i=0;i<len;i++){
-            presum+=nums[i];
-            prefix[i]=presum;
+        long sum=0;
+        for(int i:nums){
+            sum+=i;
         }
 
-        long sufsum=0;
-        for(int i=len-1;i>=0;i--){
-            sufsum+=nums[i];
-            suffix[i]=sufsum;
-        }
+        int j=0;
         int validSplits=0;
-        for(int i=0;i<len-1;i++){
-            if(prefix[i]>=suffix[i+1]){
+        long n=0;
+        while(j<len-1){
+            n+=nums[j];
+            long rem=sum-n;
+
+            if(n>=rem){
                 validSplits++;
             }
+
+            j++;
         }
         return validSplits;
     }
