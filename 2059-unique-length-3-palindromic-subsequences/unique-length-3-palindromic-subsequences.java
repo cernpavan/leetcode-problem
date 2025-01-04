f270db3f984c809a56e66327691e2b3436f55bb9
cernpavan <gunupatipavankumar@gmail.com>
2025-01-04 08:37:50 +0530

    Time: 165 ms (75.13%) | Memory: 45 MB (96.60%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        // HashMap<Character,Integer> mpps=new HashMap<>();
+        // int len=s.length();
+        // int res=0;
+        // for(int i=0;i<len;i++){
+        //     char al=s.charAt(i);
+        //     if(mpps.containsKey(al)){
+        //         if(mpps.get(al)!=i-1){
+        //             res+=i-mpps.get(al);
+        //         }
+        //     }
+        //     mpps.put(al,i);
+        // }
+        // return res;
+        int[] front=new int[26];
+        int[] end=new int[26];
+
+        for(int i=0;i<26;i++){
+            front[i]=Integer.MAX_VALUE;
+            end[i]=Integer.MIN_VALUE;
+        }
+
+        for(int i=0;i<s.length();i++){
+            int indx=s.charAt(i)-'a';
+            front[indx]=Math.min(front[indx],i);
+            end[indx]=Math.max(end[indx],i);
+        }
+
+        int res=0;
+        for(int i=0;i<26;i++){
+
+            if(front[i]==Integer.MAX_VALUE || end[i]==Integer.MIN_VALUE){
+                continue;
+            }
+
+            HashSet<Character> st=new HashSet<>();
+
+            for(int j=front[i]+1 ;j<end[i];j++){
+                st.add(s.charAt(j));
+            }
+            res+=st.size();
+        }
+        return res;
+    }
+}
