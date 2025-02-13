8cacd90d93abd574cefbe58144270d891664cce9
cernpavan <gunupatipavankumar@gmail.com>
2025-02-13 09:29:09 +0530

    Time: 122 ms (97.53%) | Memory: 69.5 MB (94.24%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,21 +1,40 @@
 import java.util.PriorityQueue;
 
+// class Solution {
+//     public int minOperations(int[] nums, int k) {
+//         PriorityQueue<Long> pq = new PriorityQueue<>();
+//         for (int num : nums) {
+//             pq.offer((long) num); 
+//         }
+
+//         long count = 0;
+//         while (pq.size() > 1 && pq.peek()<k) {
+//             long a = pq.poll();
+//             long b = pq.poll();
+//             long val = (2 * a) + b;
+//             count++;
+//             pq.offer(val);
+//         }
+
+//         return (int)count;
+//     }
+// }
 class Solution {
     public int minOperations(int[] nums, int k) {
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        for (int num : nums) {
-            pq.offer((long) num); 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            if(num<k) pq.add(num);
         }
+        int op = 0;
 
-        long count = 0;
-        while (pq.size() > 1 && pq.peek()<k) {
-            long a = pq.poll();
-            long b = pq.poll();
-            long val = (2 * a) + b;
-            count++;
-            pq.offer(val);
+        while(!pq.isEmpty()){
+            int x = pq.poll();
+            op++;
+            if(pq.isEmpty()) break;
+            int y = pq.poll();
+            long up = 2l * x + y;
+            if(up<k) pq.add((int)up);
         }
-
-        return (int)count;
+        return op;
     }
 }
